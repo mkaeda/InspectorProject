2d68c50eb00ff4af9161e600f08bdea72024a566
Makeda Morris <64229732+mkaeda@users.noreply.github.com>
2023-10-17 10:18:17 -0600

    test inspect - getting class and super name

--- src/test/InspectTest.java
+++ src/test/InspectTest.java
@@ -1,24 +1,46 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class InspectTest {
+import main.Inspector;
+
+public class InspectTest
+{
+	Inspector 				inspector	= new Inspector();
+	ByteArrayOutputStream 	outBytes 	= new ByteArrayOutputStream();
+	PrintStream				sysOut 		= System.out;
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception
+	{
+		System.setOut(new PrintStream(outBytes));
 	}
 
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception
+	{
+		System.setOut(sysOut);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInspectExtendsObject() {
+		String expectedOutput = 
+				Parent.class.getName() + "\r\n" +
+				Object.class.getName() + "\r\n";
+		
+		inspector.inspect(new Parent(), false);
+		
+		String content	= outBytes.toString();
+		assertEquals(expectedOutput, content);
 	}
 
+	class Parent {
+	}
 }
